b9a015daefef0a24f3ea4aaaa868f332d4ece5be
008 <007>
2023-11-20 23:57:59 +0800

    auto

--- src/FlashBox.ts
+++ src/FlashBox.ts
@@ -7,30 +7,30 @@ class FlashBox {
         this.plugin = plugin;
         this.plugin.eventBus.on("open-menu-content", async ({ detail }) => {
             const menu = detail.menu;
-            menu.addItem({
-                label: "插入填空闪卡B",
-                icon: "iconFlashcard",
-                click: () => {
-                    const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
-                    const range = detail?.range;
-                    if (blockID) {
-                        this.blankSpaceCard(blockID, "C");
-                        console.log(detail);
-                    }
-                },
-            });
-            menu.addItem({
-                label: "插入填空闪卡C",
-                icon: "iconFlashcard",
-                click: () => {
-                    const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
-                    const range = detail?.range;
-                    if (blockID) {
-                        this.blankSpaceCard(blockID, "C");
-                        console.log(range.cloneContents().textContent);
-                    }
-                },
-            });
+            // menu.addItem({
+            //     label: "插入填空闪卡B",
+            //     icon: "iconFlashcard",
+            //     click: () => {
+            //         const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
+            //         const range = detail?.range;
+            //         if (blockID) {
+            //             this.blankSpaceCard(blockID, "C");
+            //             console.log(detail);
+            //         }
+            //     },
+            // });
+            // menu.addItem({
+            //     label: "插入填空闪卡C",
+            //     icon: "iconFlashcard",
+            //     click: () => {
+            //         const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
+            //         const range = detail?.range;
+            //         if (blockID) {
+            //             this.blankSpaceCard(blockID, "C");
+            //             console.log(range.cloneContents().textContent);
+            //         }
+            //     },
+            // });
         });
     }
 
