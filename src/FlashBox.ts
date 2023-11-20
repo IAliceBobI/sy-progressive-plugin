2bf7f5963cf57a1a8d347fb0ddad3d54262380fe
008 <007>
2023-11-20 23:59:20 +0800

    fb

--- src/FlashBox.ts
+++ src/FlashBox.ts
@@ -1,5 +1,9 @@
 import { Plugin } from "siyuan";
 
+enum CardType {
+    B = "B", C = "C"
+}
+
 class FlashBox {
     private plugin: Plugin;
 
@@ -7,34 +11,32 @@ class FlashBox {
         this.plugin = plugin;
         this.plugin.eventBus.on("open-menu-content", async ({ detail }) => {
             const menu = detail.menu;
-            // menu.addItem({
-            //     label: "插入填空闪卡B",
-            //     icon: "iconFlashcard",
-            //     click: () => {
-            //         const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
-            //         const range = detail?.range;
-            //         if (blockID) {
-            //             this.blankSpaceCard(blockID, "C");
-            //             console.log(detail);
-            //         }
-            //     },
-            // });
-            // menu.addItem({
-            //     label: "插入填空闪卡C",
-            //     icon: "iconFlashcard",
-            //     click: () => {
-            //         const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
-            //         const range = detail?.range;
-            //         if (blockID) {
-            //             this.blankSpaceCard(blockID, "C");
-            //             console.log(range.cloneContents().textContent);
-            //         }
-            //     },
-            // });
+            menu.addItem({
+                label: "插入填空闪卡" + CardType.B.toString(),
+                icon: "iconFlashcard",
+                click: () => {
+                    const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
+                    const blank = detail?.range?.cloneContents()?.textContent ?? "";
+                    if (blockID) {
+                        this.blankSpaceCard(blockID, blank, CardType.B);
+                    }
+                },
+            });
+            menu.addItem({
+                label: "插入填空闪卡" + CardType.C.toString(),
+                icon: "iconFlashcard",
+                click: () => {
+                    const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
+                    const blank = detail?.range?.cloneContents()?.textContent ?? "";
+                    if (blockID) {
+                        this.blankSpaceCard(blockID, blank, CardType.C);
+                    }
+                },
+            });
         });
     }
 
-    private async blankSpaceCard(blockID: string, cardType: string) {
+    private async blankSpaceCard(blockID: string, cardType: string, t: CardType) {
 
     }
 }
